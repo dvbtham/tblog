69d871ae95a465586e17f466383b38a3e833b61f
dvbtham <dvbtham@gmail.com>
2016-11-09 17:31:52 +0700

    update post by the orther way :)

--- app/Post.php
+++ app/Post.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+	// it's will fill into database by using fill() function.
 	protected $fillable = ['id', 'title', 'body', 'created_at', 'updated_at'];
     protected $table = 'posts';
 }
